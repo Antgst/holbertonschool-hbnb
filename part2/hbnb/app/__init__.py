2b526e2a7d8773ca566db40999adb61775d59daf
Gwendal-B <12309@holbertonstudents.com>
2026-02-23 11:11:12 +0100

    change tree structure

---
+++ part2/hbnb/app/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask_restx import Api
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app, version='1.0', title='HBnB API',
+              description='HBnB Application API Documentation')
+
+    # Importer et enregistrer les namespaces ici
+    # from app.api.v1.users import api as users_ns
+    # api.add_namespace(users_ns, path='/api/v1/users')
+
+    return app
---
+++ part2/hbnb/app/models/place.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from app.models.base_model import BaseModel
+
+class Place(BaseModel):
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner  # Instance de User
+        self.amenities = [] # Liste d'instances d'Amenity
+        self.reviews = []   # Liste d'instances de Review
+
+    def add_amenity(self, amenity):
+        if amenity not in self.amenities:
+            self.amenities.append(amenity)
+
+    def add_review(self, review):
+        if review not in self.reviews:
+            self.reviews.append(review)
