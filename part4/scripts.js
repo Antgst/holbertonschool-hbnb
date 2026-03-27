64fa5f8e763fa064b9118fcb4571dbb35502aba8
Antgst <ant.gousset@gmail.com>
2026-03-27 10:03:09 +0100

    add: modify the script.js

--- part4/scripts.js
+++ part4/scripts.js
@@ -1,8 +1,33 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
 
-document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("login-form");
+
+  if (loginForm) {
+    loginForm.addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      try {
+      const response = await fetch("/api/v1/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password})
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        document.cookie = `token=${data.access_token}; path=/`;
+        window.location.href = "index.html";
+      } else {
+        alert("Login failed");
+      }
+    } catch (error) {
+      alert("An error occured while trying to log in.");
+    }
   });
+  }
+});
